a8221f321cf316fbc676d0523c9164a09616768d
Gunjan <glunkad007@gmail.com>
2023-11-16 23:00:31 +0530

    Automatic update: 16-11-2023 at 23:00:31

--- .bin/dotsync.sh
+++ .bin/dotsync.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
-
-# Sync dotfiles repo and ensure that dotfiles are tangled correctly afterward
+#!/bin/bash
 
 GREEN='\033[1;32m'
 BLUE='\033[1;34m'
 RED='\033[1;30m'
 NC='\033[0m'
 
-# Navigate to the directory of this script (generally ~/.dotfiles/.bin)
-cd $(dirname $(readlink -f $0))
-cd ..
+cd "$(dirname "$(readlink -f "$0")")"
+DOTFILES_DIR="$(dirname "$PWD")"
+
+timestamp() {
+  date +"%d-%m-%Y at %T"
+}
 
 echo -e "${BLUE}Stashing existing changes...${NC}"
 stash_result=$(git stash push -m "sync-dotfiles: Before syncing dotfiles")
@@ -30,11 +31,18 @@ if [ $needs_pop -eq 1 ]; then
 fi
 
 unmerged_files=$(git diff --name-only --diff-filter=U)
-if [ ! -z $unmerged_files ]; then
+if [ ! -z "$unmerged_files" ]; then
    echo -e "${RED}The following files have merge conflicts after popping the stash:${NC}"
    echo
-   printf %"s\n" $unmerged_files  # Ensure newlines are printed
+   printf "%s\n" "$unmerged_files"  # Ensure newlines are printed
 else
    # Run stow to ensure all new dotfiles are linked
-   stow .
+   stow -d "$DOTFILES_DIR" .
+fi
+
+if [[ $(git status --porcelain) ]]; then
+    echo -e "${BLUE}Pushing changes to the dotfiles repo...${NC}"
+    git add .
+    git commit -m "Automatic update: $(timestamp)"
+    git push origin main
 fi
